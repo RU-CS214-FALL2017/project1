996e8fbad917d5d54c839706e8bf3f87102d8522
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2017-10-21 18:32:42 -0400

    Initial Commit

---
+++ porject1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  porject1
+//
+//  Created by Ammaar Iqbal on 10/21/17.
+//  Copyright © 2017 qwezey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
