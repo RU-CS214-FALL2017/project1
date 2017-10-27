5c3cf2fa5cb3fb7f2699ffc4112f51840c859a24
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2017-10-27 09:48:49 -0400

    updated free.c and free.h

--- project1/free.c
+++ project1/free.c
@@ -1,11 +1,17 @@
-// For Coasta, you can include header files you need here.
+// For Coasta, you edit this file and free.h as you wish,
+// just make sure these two functions are present. I would
+// advise making helper functions that accept void pointers
+// and these functions call those funcitons. The reason I'm
+// doing this is becasue I realized this makes the caller
+// life easier by not having to specify the size of the data
+// type.
 
 // Frees <alloc>[i] (0 <= i < <x>) and <alloc>.
-void doubleFree(void ** alloc, int x) {
+void doubleFree(char ** alloc, int x) {
     
 }
 
 // Frees <alloc>[i][j] (0 <= i < <x>, 0 <= j < <y>) and <alloc>.
-void tripleFree(void *** alloc, int x, int y) {
+void tripleFree(char *** alloc, int x, int y) {
     
 }
