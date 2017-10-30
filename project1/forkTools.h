ac5b078a6b1de9b5f745563d9d972074f941a7a7
Ammaar Muhammad Iqbal <ammaar.iqbal@gmail.com>
2017-10-29 22:59:57 -0400

    added struct csvdir

--- project1/forkTools.h
+++ project1/forkTools.h
@@ -1,6 +1,21 @@
 #ifndef forkTools_h
 #define forkTools_h
 
+// Represents a processed directory.
+struct csvDir {
+    
+    pid_t * pid;
+    char * path;
+    
+    pid_t * subChildPids;
+    struct csvDir ** subDirs;
+    unsigned int * numSubDirs;
+    
+    pid_t * csvChildPids;
+    char ** csvPaths;
+    unsigned int * numCsvPaths;
+};
+
 int processCsvDir(const char * path, struct csvDir * * info, const char * columnHeader, const char * outputDir);
 pid_t * multiFork(int num);
 void * pipeDataToChildren(const void * source, size_t size, int pipedFd[2], int isParent, int numChildren);
